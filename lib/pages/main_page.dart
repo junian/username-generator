0f71e85b1f0aa3bef1904d8730d1ae762f62d403
Junian Triajianto <author@junian.dev>
2024-11-08 17:32:45 +0700

    Move main page

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:username_generator/app_states/main_app_state.dart';
+import 'package:username_generator/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Username Generator',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
