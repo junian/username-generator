d3ea19618c10cf7d04fc30d0d5e6fedc80f4eef1
Junian Triajianto <author@junian.dev>
2024-11-08 16:27:48 +0700

    Add navigation

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,66 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    
+    Widget page;
+    if(selectedIndex == 0) {
+      page = GeneratorPage();
+    } else if (selectedIndex == 1)
+      // ignore: curly_braces_in_flow_control_structures
+      page = Placeholder();
+    else {
+      throw UnimplementedError('no Widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home), 
+                    label: Text('Home')
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite), 
+                    label: Text('Favorites')
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value){
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
